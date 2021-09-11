e773f044d3649d0a962e3ba2f57523a967eefe23
KatySpielberg <54340464+KatySpielberg@users.noreply.github.com>
2021-09-11 21:57:16 +0300

    Add files via upload
    reload project

---
+++ UsersTestApi/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UsersTest.Models;
+
+namespace UsersTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<UserModel> users = new List<UserModel>();
+
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserModel> Get()
+        {
+            return users;
+        }
+
+        [HttpPost]
+        public object Post([FromBody] UserModel user)
+        {
+            if (user != null)
+            {
+                user.ID = Guid.NewGuid().ToString();
+                users.Add(user);
+
+                return new { id = user.ID };
+            }
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public object Delete(string id)
+        {
+            if (users.Any(user => user.ID == id))
+            {
+                users.RemoveAll(user => user.ID == id);
+                return new { success = true };
+            }
+            return new { success = true };
+        }
+    }
+}
---
+++ UsersTestApi/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersTest.Models
+{
+    public class UserModel
+    {
+        public string ID { get; set; }
+        [Required]
+        [StringLength(maximumLength: 50, MinimumLength = 2)]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
